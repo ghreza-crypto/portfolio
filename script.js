7eab5b5de92e295428fe92d9df95ad2844e4bae2
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-09 05:20:43 +0430

    Fix linter errors

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const closeNavbarMenue = document.querySelector('#close');
+const humbarger = document.querySelector('#humbarger');
+
 function menue() {
-    document.querySelector('#navbarMenue').classList.add('active');
+  document.querySelector('#navbarMenue').classList.add('active');
 }
-function menuseClose() {
-  
-    document.querySelector('#navbarMenue').classList.remove('active');
+function menueClose() {
+  document.querySelector('#navbarMenue').classList.remove('active');
 }
+closeNavbarMenue.addEventListener('click', menueClose);
+humbarger.addEventListener('click', menue);
+document.querySelectorAll('.navbarLinks').forEach((navbarLink) => navbarLink.addEventListener('click', menueClose));
