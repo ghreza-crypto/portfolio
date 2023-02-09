0fdf573dd8eb5fef84f852f9cf4ad4522a034de1
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-09 07:00:52 +0430

    Fix close icon and background scroll

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const humbarger = document.querySelector('#humbarger');
 
 function menue() {
   document.querySelector('#navbarMenue').classList.add('active');
+  document.getElementById('body').style.overflow = 'hidden';
 }
 function menueClose() {
   document.querySelector('#navbarMenue').classList.remove('active');
+  document.getElementById('body').style.overflow = 'scroll';
 }
 closeNavbarMenue.addEventListener('click', menueClose);
 humbarger.addEventListener('click', menue);
