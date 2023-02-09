18cdeee62fa9af76be297193cc19ec828f0f30a9
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-09 19:42:02 +0430

    Update style file for background blur in mobile menue section

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const humbarger = document.querySelector('#humbarger');
 function menue() {
   document.querySelector('#navbarMenue').classList.add('active');
   document.getElementById('body').style.overflow = 'hidden';
+  document.getElementById('main').style.filter = 'blur(4px)';
 }
 function menueClose() {
   document.querySelector('#navbarMenue').classList.remove('active');
   document.getElementById('body').style.overflow = 'scroll';
+  document.getElementById('main').style.filter = 'none';
 }
 closeNavbarMenue.addEventListener('click', menueClose);
 humbarger.addEventListener('click', menue);
