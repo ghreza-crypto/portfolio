6ae2fffaf5c1ec8bcad22d396aabebeecfbcc571
everinurmind <nurbol.sultanov@gmail.com>
2023-02-10 12:59:08 -0600

    Add preserve-data file for saving user input

---
+++ scripts/preserve-data.js
@@ -0,0 +1,9 @@
+const name = document.getElementById('name');
+const message = document.getElementById('message');
+const email = document.getElementById('email');
+
+let form= {
+    name: '',
+    email:'',
+    message: '',
+  };
