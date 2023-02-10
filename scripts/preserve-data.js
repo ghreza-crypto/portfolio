7638556b65fd55bdca572a29aa506290413429a9
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-11 00:40:33 +0430

    Add initial part to preserv data

--- scripts/preserve-data.js
+++ scripts/preserve-data.js
@@ -1,9 +1,28 @@
-const name = document.getElementById('name');
-const message = document.getElementById('message');
-const email = document.getElementById('email');
+let contactFormName = document.getElementById('senderName');
+let contactFormMessage = document.getElementById('message');
+let contactFormEmail = document.getElementById('email');
+let saved=localStorage.getItem('contactForm');
 
 let form= {
     name: '',
     email:'',
     message: '',
   };
+
+  if (saved) {
+    form = JSON.parse(saved);
+    contactFormName.value = form.name;
+    contactFormMessage.value=form.message;
+    contactFormEmail.value = form.email;
+  }
+
+  saveForm = (e) => {
+    const element = e.target;
+    form[element.name] = element.value;
+    localStorage.setItem('contactForm', JSON.stringify(form));
+  };
+
+  
+contactFormName.addEventListener('change', saveForm);
+contactFormEmail.addEventListener('change', saveForm);
+contactFormMessage.addEventListener('change', saveForm);
